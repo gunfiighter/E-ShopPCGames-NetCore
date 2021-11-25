010f182a1179a77f3ec898ac79e0ea1ac57b0bfb
gunfiighter <giniyatulinm@mail.ru>
2021-11-26 02:04:20 +0500

    Db Connection And Category View

---
+++ BuiMuiGaim/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using BuiMuiGaim.Data;
+using BuiMuiGaim.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuiMuiGaim.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+    }
+}
---
+++ BuiMuiGaim/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+using BuiMuiGaim.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BuiMuiGaim.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
