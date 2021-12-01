54b3d086f6e81f9813b5d5bd5425c7ca1f019afe
gunfiighter <giniyatulinm@mail.ru>
2021-12-01 16:00:08 +0500

    Added products on Home Page

--- BuiMuiGaim/Controllers/HomeController.cs
+++ BuiMuiGaim/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using BuiMuiGaim.Models;
+﻿using BuiMuiGaim.Data;
+using BuiMuiGaim.Models;
+using BuiMuiGaim.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -12,15 +15,21 @@ namespace BuiMuiGaim.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _db;
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM()
+            {
+                Products = _db.Product.Include(x => x.Category).Include(x => x.ApplicationType),
+                Categories = _db.Category
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
