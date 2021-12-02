fa51626f92cc5d5fef6d82dc4239231317e28b26
gunfiighter <giniyatulinm@mail.ru>
2021-12-02 15:00:17 +0500

    Added Shopping Card View

---
+++ BuiMuiGaim/Controllers/CartController.cs
@@ -0,0 +1,36 @@
+﻿using BuiMuiGaim.Data;
+using BuiMuiGaim.Models;
+using BuiMuiGaim.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuiMuiGaim.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CartController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppingCartList= new List<ShoppingCart>();
+
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null &&
+                HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                //session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(x => x.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(x => prodInCart.Contains(x.Id));
+            return View(prodList);
+        }
+    }
+}
