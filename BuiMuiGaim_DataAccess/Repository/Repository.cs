35822703f4fb30e124ca5b560b9eccc0a494931d
gunfiighter <giniyatulinm@mail.ru>
2021-12-09 23:22:37 +0500

    Added Realization Of Repository pattern

---
+++ BuiMuiGaim_DataAccess/Repository/Repository.cs
@@ -0,0 +1,94 @@
+﻿using BuiMuiGaim_Data;
+using BuiMuiGaim_DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuiMuiGaim_DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Find(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public T FirstOrDefault(System.Linq.Expressions.Expression<Func<T, bool>> filter = null, string includeProperties = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }           
+            if (!isTracking)
+            {
+                query.AsNoTracking();
+            }
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll(
+            System.Linq.Expressions.Expression<Func<T, bool>> filter = null, 
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, 
+            string includeProperties = null, 
+            bool isTracking = true
+            )
+        {
+            IQueryable<T> query = dbSet;
+            if(filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if(includeProperties != null)
+            {
+                foreach(var includeProp in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
+            if(orderBy != null)
+            {
+                query = orderBy(query);
+            }
+            if(!isTracking)
+            {
+                query.AsNoTracking();
+            }
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
