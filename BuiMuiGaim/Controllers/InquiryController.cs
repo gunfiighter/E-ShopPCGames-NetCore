e6772698253b5ec30c9a39f1358b35995852af52
gunfiighter <giniyatulinm@mail.ru>
2021-12-11 17:48:40 +0500

    Added View Model for Inquiries. Inquiry Controller for this.

---
+++ BuiMuiGaim/Controllers/InquiryController.cs
@@ -0,0 +1,26 @@
+﻿using BuiMuiGaim_DataAccess.Repository.IRepository;
+using BuiMuiGaim_Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BuiMuiGaim.Controllers
+{
+    public class InquiryController : Controller
+    {
+        private readonly IInquiryHeaderRepository _inqHRepo;
+        private readonly IInquiryDetailRepository _inqDRepo;
+        public InquiryController(IInquiryDetailRepository inqDRepo, IInquiryHeaderRepository inqHRepo)
+        {
+            _inqDRepo = inqDRepo;
+            _inqHRepo = inqHRepo;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BuiMuiGaim_Models/ViewModels/InquiryVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuiMuiGaim_Models.ViewModels
+{
+    public class InquiryVM
+    {
+        public InquiryHeader InquiryHeader { get; set; }
+        public IEnumerable<InquiryDetail> InquiryDetails { get; set; }
+
+    }
+}
